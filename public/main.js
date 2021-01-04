63d93499844fbc467749b6a3d9b42a687dead0c9
GeronimoJohn <johnageronimo1@gmail.com>
2021-01-04 14:45:44 +1300

    rendered data from foursquare and openweahter

--- public/main.js
+++ public/main.js
@@ -70,8 +70,10 @@ const getForecast = async () => {
 const renderVenues = (venues) => {
   $venueDivs.forEach(($venue, index) => {
     // Add your code here:
-
-    let venueContent = "";
+    const venue = venues[index];
+    const venueIcon = venue.categories[0].icon;
+    const venueImgSrc = `${venueIcon.prefix}bg_64${venueIcon.suffix}`;
+    let venueContent = createVenueHTML(venue.name, venue.location, venueImgSrc);
     $venue.append(venueContent);
   });
   $destination.append(`<h2>${venues[0].location.city}</h2>`);
@@ -79,8 +81,7 @@ const renderVenues = (venues) => {
 
 const renderForecast = (day) => {
   // Add your code here:
-
-  let weatherContent = "";
+  let weatherContent = createWeatherHTML(day);
   $weatherDiv.append(weatherContent);
 };
 
@@ -89,8 +90,8 @@ const executeSearch = () => {
   $weatherDiv.empty();
   $destination.empty();
   $container.css("visibility", "visible");
-  getVenues();
-  getForecast();
+  getVenues().then((venues) => renderVenues(venues));
+  getForecast().then((forecast) => renderForecast(forecast));
   return false;
 };
 
